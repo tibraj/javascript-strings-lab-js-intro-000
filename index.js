e6706ae8c65f312cfccfc2aea39b0ddb1d9d90c2
Taulant Ibraj <tibraj94@gmail.com>
2019-12-24 14:54:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = 'Hello, Neil deGrasse Tyson!';
 
 var topic = "space";
 
-var conversation = `${greetSpecialGuest}` "Let\'s talk about `${topic}`." ;
+var conversation = `${greetSpecialGuest}`, "Let\'s talk about `${topic}`." ;
