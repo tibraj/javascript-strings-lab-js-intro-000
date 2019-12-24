f0a4caa594e3a03061cbfbed8712b503a7377fad
Taulant Ibraj <tibraj94@gmail.com>
2019-12-24 14:43:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "Hello";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = `${greeting}, ${specialGuest}!`;
+var greetSpecialGuest = 'Hello, Neil Degrasse Tyson!';
 
-var topic = "Let\'s talk about space."; 
+var topic = "Let\'s talk about space.";
 
 var conversation = `${greetSpecialGuest}, ${topic}`;
